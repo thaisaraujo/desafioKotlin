36d649d3bbb5fd56ac4536a1c10849cf3632888d
thaisaraujo <thaisaraujo.hp@hotmail.com>
2020-09-25 17:16:32 -0300

    modulo h

--- src/br/com/digitalhouse/Curso.kt
+++ src/br/com/digitalhouse/Curso.kt
@@ -1,10 +1,25 @@
 package br.com.digitalhouse
 
 class Curso (val nome: String, val codigoCurso: Int, var professorTitular: ProfessorTitular,
-             var professorAdjunto: ProfessorAdjunto, qtdMaxAlunos: Int, listaDeAlunos : MutableList<Aluno>) {
+             var professorAdjunto: ProfessorAdjunto, var qtdMaxAlunos: Int, val listaDeAlunos : MutableList<Aluno>) {
 
     override fun equals(other: Any?): Boolean {
         return !(other == null || other != codigoCurso)
+    }
+
+
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+        if(!listaDeAlunos.contains(umAluno)) {
+            listaDeAlunos.add(umAluno)
+            return true
+        } else {
+            return false
+        }
+    }
+
+    fun excluirUmAluno(umAluno: Aluno){
+        if(listaDeAlunos.contains(umAluno))
+            listaDeAlunos.remove(umAluno)
     }
 
 
